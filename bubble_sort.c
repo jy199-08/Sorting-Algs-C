7d56456dfb1c3d6fddd46efb774eb375e1c4cf3b
Jay <jycheah.cjy@gmail.com>
2026-09-20 15:01:04 +0800

    Initial commit

---
+++ bubble_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void bubble_sort(int array[], int length);
+
+int main(void)
+{
+    int numbers[] = {3, 4, 7, 5, 6, 1, 8, 9, 2};
+    int length = sizeof(numbers) / sizeof(numbers[0]);  // works here: numbers is a real array
+
+    bubble_sort(numbers, length);
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+void bubble_sort(int array[], int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        for (int j = 0; j < length - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
